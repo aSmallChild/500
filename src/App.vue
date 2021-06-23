6637ea9f9d17f2b8a9500007d46e1a6e5650d85d
Simeon <simeon@legit.nz>
2021-06-23 21:57:25 +1200

    added vue

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>App</div>
+</template>
+
+<script>
+export default {
+    name: 'App',
+    components: {},
+};
+</script>
+
+<style lang="scss">
+#app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #fff;
+    margin-top: 60px;
+}
+
+body {
+    background: #000;
+}
+</style>
