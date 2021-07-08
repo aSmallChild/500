ad5f0d0fa6daa33b199eb7fe13af75c669a3a733
Simeon <simeon@legit.nz>
2021-07-08 21:00:45 +1200

    styling

---
+++ components/CardGroup.vue
@@ -0,0 +1,46 @@
+<template>
+    <div></div>
+</template>
+
+<script>
+export default {
+    props: {
+        cards: Array
+    },
+    computed: {
+        _cards() {
+            this.cards.forEach((cardSvg/*, index*/) => {
+                // todo use the index to put cards in order
+                // todo if a card is moving remember its original position and animate
+                if (cardSvg.svg.parentElement !== this.$el) {
+                    this.$el.appendChild(cardSvg.svg);
+                }
+            });
+            return true;
+        }
+    },
+    methods: {
+        hasCard(svg) {
+            for (const cardSvg of this.cards) {
+                if (cardSvg.svg === svg) {
+                    return true;
+                }
+            }
+            return false;
+        },
+        addCard(cardSvg) {
+            if (!cardSvg.svg.parentElement) {
+                this.$el.appendChild(cardSvg.svg);
+                return;
+            }
+            cardSvg.animateSiblings(() => {
+                this.$el.appendChild(cardSvg.svg);
+            });
+        },
+    },
+};
+</script>
+
+<style lang="scss">
+
+</style>
--- src/App.vue
+++ src/App.vue
@@ -3,14 +3,15 @@
     <svg width="0" height="0">
         <defs ref="svgDefs"></defs>
     </svg>
-    <card-table ref="table" class="animate-all"></card-table>
-    <fan ref="fan1" class="animate-all"></fan>
-    <fan ref="fan2" class="animate-all"></fan>
+    <card-table ref="table" class="animate-cards"></card-table>
+    <card-group ref="fan1" class="animate-cards fan"></card-group>
+    <card-group ref="fan2" class="animate-cards fan"></card-group>
+<!--    <card-group class="animate-cards fan" v-for="hand in hands" :cards="hand" :key="hand"></card-group>-->
 </template>
 
 <script>
-import Table from '../components/Table.vue';
-import Fan from '../components/Fan.vue';
+import CardTable from '../components/CardTable.vue';
+import CardGroup from '../components/CardGroup.vue';
 import CardSVGBuilder from './view/CardSVGBuilder.js';
 import CardSVG from './view/CardSVG.js';
 import DeckConfig from '../src/game/model/DeckConfig.js';
@@ -23,8 +24,8 @@ const config = new DeckConfig(OrdinaryNormalDeck.config);
 export default {
     name: 'App',
     components: {
-        'card-table': Table,
-        'fan': Fan,
+        CardTable,
+        CardGroup,
     },
     data() {
         return {
@@ -45,7 +46,7 @@ export default {
             // });
             cardSvg.svg.addEventListener('click', () => {
                 const moveToTable = cardSvg.svg.parentElement !== this.$refs.table.$el;
-                cardSvg.animateSiblings(() => {
+                cardSvg.animateTo(() => {
                     if (moveToTable) {
                         this.$refs.table.playCard(cardSvg);
                     } else {
@@ -92,14 +93,21 @@ html {
     background: #151;
 }
 
+$cardHeight: 150px;
+
 .card {
-    border: solid 2px var(--suit-black);
+    border: solid 1px var(--suit-black);
     border-radius: 4px;
     background: #111;
-    //width: 2.5in;
-    //height: 3.5in;
-    width: 125px;
-    height: 175px;
+    height: $cardHeight;
+    margin: 0.3%;
+    padding: 0.1%;
+    box-shadow: #000a 2px 2px 4px;
+    fill: var(--suit-black);
+    stroke: var(--suit-black);
+}
+
+.black {
     fill: var(--suit-black);
     stroke: var(--suit-black);
 }
@@ -119,7 +127,44 @@ html {
     stroke: var(--suit-green);
 }
 
-.animate-all .card {
-    transition: ease-out all 777ms;
+.animate-cards .card {
+    transition: ease-out transform 777ms;
+}
+
+@mixin fan($count, $angle) {
+    $offset: -$angle / 2;
+    @for $i from 1 through $count {
+        $increment: $angle / ($count + 1);
+        .card:nth-child(#{$i}) {
+            transform: translate(-50%, -50%) rotate($offset + $increment * $i);
+        }
+    }
+}
+
+@mixin allTheFans($counts, $angle) {
+    $offset: -$angle / 2;
+    @for $count from 1 through $counts {
+        .card:first-child:nth-last-child(#{$count}),
+        .card:first-child:nth-last-child(#{$count}) ~ .card {
+            @for $i from 1 through $count {
+                $increment: $angle / ($count + 1);
+                &:nth-child(#{$i}) {
+                    transform: translate(-50%, 6%) rotate($offset + $increment * $i);
+                }
+            }
+        }
+    }
+}
+
+.fan {
+    width: $cardHeight * 2.2;
+    height: $cardHeight * 1.4;
+    position: relative;
+    @include allTheFans(16, 90deg);
+
+    .card {
+        position: absolute;
+        transform-origin: 50% 125%;
+    }
 }
 </style>
