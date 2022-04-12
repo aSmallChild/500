c389b82128cce4c0731ba3e6048aaf778f159ac6
Simeon <simeon@legit.nz>
2022-04-12 21:31:47 +1200

    worker endpoints

--- server/cloudflare-worker/src/index.js
+++ server/cloudflare-worker/src/index.js
@@ -1,4 +1,6 @@
-import createSession from './createSession.js';
+import {jsonRequest, jsonResponse} from './util.js';
+
+import {lobbyTypes} from './Lobby.js';
 
 export default {
     async fetch(request, env) {
@@ -6,82 +8,92 @@ export default {
     },
 };
 
+async function handleRequest(request, env) {
+    // POST /lobby {lobbyPassword, type, user}
+    //  -> DO/lobby_create
+
+    // POST /lobby/lobbyId {lobbyPassword, user}
+    //  -> DO/client_create
+
+    // GET /session/lobbyId/clientId
+    //  -> DO/client_connect/lobbyId/clientId
+
+    const url = new URL(request.url);
+    if (url.pathname == '/lobby') return await createLobby(request, env);
+
+    const [_, path, lobbyId, clientId] = url.pathname.split('/');
+    if (!lobbyId || lobbyId.length > 6) return new Response('bad lobbyId', {status: 400});
+    if (path == 'lobby') return await createClient(request, lobbyId);
+    if (path == 'session') return await createSession(request, lobbyId, clientId);
+    return new Response('bad url', {status: 404});
+}
+
+async function createLobby(request, env) {
+    const [json, response] = jsonRequest(request);
+    if (response) return response;
+
+    if (!lobbyTypes.includes(json.lobby.type)) {
+        return jsonResponse({error: 'bad lobby type'}, 400);
+    }
+
+    return await findAvailableLobbyAndSetItUp(request, env);
+}
+
+async function createClient(request, lobbyId) {
+    const [json, response] = jsonRequest(request);
+    if (response) return response;
+    request.url = 'https://.../client_create';
+    return getLobby(lobbyId, env).fetch(request);
+}
+
+async function createSession(request, lobbyId, clientId) {
+    if (request.method != 'GET') return new Response('bad method', {status: 405});
+    if (!clientId) return new Response('bad clientId', {status: 400});
+    request.url = `https://.../session_create/${lobbyId}/${clientId}`;
+    return getLobby(lobbyId, env).fetch(request);
+}
+
 const getLobby = (key, env) => env.LOBBY.get(env.LOBBY.idFromName(key));
 
-function websocketUpgrade() {
-    const {0: client, 1: server} = new WebSocketPair();
-    server.accept();
-    return [new Response(null, {status: 101, webSocket: client}), server];
-}
-
-async function handleRequest(request, env) {
-    // requests
-        // POST /lobby
-            // body:
-                // lobby: (required)
-                    // name: string (required) lobby identifier
-                    // type: string (required if lobby doesn't exist, ignored if it does) whether it's a doodle or game
-                    // password: string (optional) set on lobby if its being created, checked if the lobby already exists
-                // user: (optional)
-                    // username|google_id|auth0_id|some_kind_of_id (required)
-                    // password (optional) (set if new, check if already exists)
-            // response
-                // session_id (string) token can be used for the session endpoint to open up a socket connection
-                // lobby_id (display only)
-                // username (display only, this could be derived from the google_id they login)
-                // error
-
-        // socket /lobby/<lobby_id>/<session_id>
-
-
-        // lobby passes some kind of state persistencey interface with a get and set method for storing stuff in durable objects
-        // lobby calls channel.onSession(socket => {...}) whenever someone connects
-
-    const key = 'A';
-    return getLobby(key, env).fetch(request);
-}
-
-export class Lobby {
-    constructor(state, env) {
-        this.state = state;
-        this.taken = false;
-        this.sessionCount = 0;
-    }
-
-    handleSession(request, session) {
-        this.sessionCount++;
-        session.onClose = () => this.sessionCount--;
-    }
-
-    async fetch(request) {
-        const upgradeHeader = request.headers.get('Upgrade');
-        if (upgradeHeader) {
-            if (upgradeHeader !== 'websocket') {
-                return new Response('Expected Upgrade: websocket', {status: 426});
-            }
-
-            return this.handleWebsocketUpgrade();
+async function findAvailableLobbyAndSetItUp(request, env) {
+    request.url = 'https://.../create_lobby';
+    let response;
+    for (let i = 0; i < 5; i++) {
+        const lobby = getLobby(createLobbyId(), env);
+        response = await lobby.fetch(request);
+        if (response.status != 409) {
+            return response;
         }
-
-        return new Response('Not found!', {status: 404});
     }
-
-    handleWebsocketUpgrade() {
-        const upgradeHeader = request.headers.get('Upgrade');
-        if (upgradeHeader && upgradeHeader !== 'websocket') {
-            return new Response('Expected Upgrade: websocket', {status: 426});
-        }
-        const [response, session] = createSession();
-        this.handleSession(request, session);
-        return response;
-    }
+    return response;
 }
 
-function jsonResponse(data, status = 200) {
-    return new Response(JSON.stringify(data), {
-        status,
-        headers: {
-            'content-type': 'application/json;charset=UTF-8',
-        },
-    });
+function getRandomLetters(length) {
+    let result = '';
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    for (let i = 0; i < length; i++) {
+        result += characters.charAt(Math.floor(Math.random() * characters.length));
+    }
+    return result;
+}
+
+function checkLobbyId(code) {
+    const a = 'NUIN';
+    let matches = 0;
+    for (let i = 0; i < a.length; i++) {
+        if (code[i] == a[i]) {
+            matches++;
+        }
+    }
+    return matches != 1;
+}
+
+function createLobbyId() {
+    for (let i = 0; i < 100; i++) {
+        const code = getRandomLetters(4);
+        if (checkLobbyId(code)) {
+            return code;
+        }
+    }
+    return null;
 }
---
+++ server/cloudflare-worker/src/util.js
@@ -0,0 +1,19 @@
+export function jsonRequest(request) {
+    if (request.method != 'POST') {
+        return [null, new Response('bad method', {status: 405})];
+    }
+    try {
+        return [JSON.parse(request.body)];
+    } catch (err) {
+        return [null, new Response('bad json', 400)];
+    }
+}
+
+export function jsonResponse(data, status = 200) {
+    return new Response(JSON.stringify(data), {
+        status,
+        headers: {
+            'content-type': 'application/json;charset=UTF-8',
+        },
+    });
+}
