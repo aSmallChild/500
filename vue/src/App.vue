decf14d9e5409ccd59ee47b7718471fdf34c5b6f
aSmalLChild <totally@legit.nz>
2022-07-24 14:31:39 +1200

    Remove theme editor.

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,20 +1,17 @@
-<template>
-    <n-config-provider :theme="darkTheme" :theme-overrides="darkThemeOverrides">
-        <n-theme-editor>
-            <router-link to="/" v-if="$router.currentRoute.value.fullPath !== '/'" custom v-slot="{navigate}">
-                <n-button style="position: absolute; margin: 16px;" @click="navigate">Menu</n-button>
-            </router-link>
-            <router-view/>
-        </n-theme-editor>
-        <n-global-style/>
-    </n-config-provider>
-</template>
-
 <script setup>
-import {NButton, NConfigProvider, NThemeEditor, NGlobalStyle} from 'naive-ui';
-import {darkTheme} from 'naive-ui';
+import {darkTheme, NButton, NConfigProvider, NGlobalStyle} from 'naive-ui';
 import darkThemeOverrides from './theme/dark.json';
 </script>
+
+<template>
+  <n-config-provider :theme="darkTheme" :theme-overrides="darkThemeOverrides">
+    <router-link to="/" v-if="$router.currentRoute.value.fullPath !== '/'" custom v-slot="{navigate}">
+      <n-button style="position: absolute; margin: 16px;" @click="navigate">Menu</n-button>
+    </router-link>
+    <router-view/>
+    <n-global-style/>
+  </n-config-provider>
+</template>
 
 <style lang="scss">
 @use "css/base";
